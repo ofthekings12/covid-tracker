c24d4e212d4a2983e8791194893c3eeda0f25f80
Mikko De Los Reyes <mikkode212@gmail.com>
2022-02-26 11:08:18 -0500

    added some basic css, and created a dropdown using useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "Canada", "India"]);
+
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map(country => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+
+
+
+            {/* <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title & Select input dropdown field */}
+
+      {/* Info boxes */}
+      {/* Info boxes */}
+      {/* Info boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
