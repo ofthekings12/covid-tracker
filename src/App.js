39bf69e4d19f8ee9a444a5989446db16d521ec56
Mikko De Los Reyes <mikkode212@gmail.com>
2022-02-26 16:06:55 -0500

    map now zooms on country when selected

--- src/App.js
+++ src/App.js
@@ -58,6 +58,9 @@ function App() {
     .then((data) => {
       setInputCountry(countryCode);
       setCountryInfo(data);
+
+      setMapCenter([data.countryInfo.lat, data.countryInfo.long])
+      setMapZoom(4);
     })
 
   };
